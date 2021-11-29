e00d3a9fa1c38db61a994a944c225296c75f0a74
Arjun Pansare <arjun_pansare@yahoo.com>
2021-11-30 00:54:52 +0530

    Delayed rendering

--- src/Components/Solve.js
+++ src/Components/Solve.js
@@ -32,30 +32,32 @@ const Solve = (props) => {
             len=len+10
         }
     }
-
     const [solution, setSolution] = useState();
+    const [waiting, setWaiting] = useState(0);
     const url="https://api.wolframalpha.com/v2/query?input="+eq+"&output=json&appid=77QY3U-QR78VRQUR7";
     console.log(url);
+    setTimeout(()=>setWaiting(1),5000)
     useEffect(() => {
         axios.get("https://cors-anywhere.herokuapp.com/"+url)
             .then(response => setSolution(response));
-        
     }, []);
 
     return (
+        (waiting)?(
         <div className="solution home">
+            <h1>{waiting}</h1>
             {console.log(solution)}
             <div className="box">
                 <h1>Solution: </h1>
                 <h6>Entered equation: {reqEquation}</h6>
                 <br />
                 {/* x=1/2 */}
-                {setTimeout(()=>{console.log("waiting");},5000)}
                 {console.log(solution.data.queryresult.pods[4].subpods[0].img.alt)}
-                {/* <p>{ans}</p> */}
+                <p>{solution.data.queryresult.pods[4].subpods[0].img.alt}</p>
                 {/* queryresult.pods[4].subpods[0].img */}
             </div>
         </div>
+    ):(<>{waiting}</>)
     )
 }
 
