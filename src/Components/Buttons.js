a795a9aad40d29bc647b131f364bfd7925ac7fcd
Arjun Pansare <arjun_pansare@yahoo.com>
2021-11-30 03:38:24 +0530

    removed visualise button from home

--- src/Components/Buttons.js
+++ src/Components/Buttons.js
@@ -8,10 +8,6 @@ const Buttons = (props) => {
       navigate('/solve',{state:props});
     }
 
-    const visEq=()=>{
-      navigate('/vis',{state:props});
-    }
-
     return (
         <div className="flex flex-row justify-end mx-auto">
                 {/* {console.log(props)} */}
@@ -21,13 +17,6 @@ const Buttons = (props) => {
                     className="btn btn-success mt-5 ms-3"
                     value="Solve"
                   />
-                </div>
-                <div onClick={()=>{visEq()}}>
-                <input
-                  type="button"
-                  className="btn btn-primary mt-4 ms-1"
-                  value="Visualize"
-                />
                 </div>
         </div>
     )
